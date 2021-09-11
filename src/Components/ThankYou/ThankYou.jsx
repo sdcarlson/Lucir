368a3c90ed10e5bdadc8429a7be60469d55ef6ab
tristenlol <90505454+tristenlol@users.noreply.github.com>
2021-09-11 03:20:45 -0700

    Add files via upload

--- src/Components/ThankYou/ThankYou.jsx
+++ src/Components/ThankYou/ThankYou.jsx
@@ -9,6 +9,8 @@ function ThankYou() {
         <div>
             <div className={classes.toolbar} />
             <h1>Thank You</h1>
+            <p id="thanksmessage">We appreciate every visit to our store. We thank you graciously for your contribution to our cause. We will never stop doing what we do, and we can only do it with your support. We have a long road ahead of us, yet we hardy heads must trudge on. Godspeed, glorious instance of mankind. </p>
+            <p id ="bottomtext"> ce n'est pas une vie à vivre sans difficulté </p>
         </div>
     )
 }
