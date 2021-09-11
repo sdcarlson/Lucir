1bb000904bf7050a1879d8fdac8fa8d3eff8e913
tristenlol <90505454+tristenlol@users.noreply.github.com>
2021-09-11 03:20:17 -0700

    Add files via upload

--- src/Components/VideoBackground/VideoBackground.jsx
+++ src/Components/VideoBackground/VideoBackground.jsx
@@ -5,20 +5,24 @@ import { Button } from '@material-ui/core';
 import './HeroSection.css';
 import video from '../../assets/waves.mp4'
 import { Link, useLocation } from 'react-router-dom';
+import lucir from '../../assets/lucir.png';
+
 
 
 function VideoBackground() {
+    const image = lucir;
     return (
         <div>
-            <div className='hero-container'>
+            <div className='hero-container'>   
                 <video src={video} autoPlay loop muted />
-                <h1>WELCOME TO LUCIR</h1>
-                <p>What are you waiting for?</p>
-                <Button component={Link} to="/shop" className='btn' variant="contained" color="secondary" style={{ maxWidth: '130px', maxHeight: '70px', minWidth: '130px', minHeight: '70px' }} >
-                    Shop Now
+                <span id="welcomeTo">Presenting </span><img id="lucir" src={image} alt="Link to store" />
+                <Button component={Link} to="/shop" className='btn' variant="contained" color='mintgreen' style={{ marginTop: '60px', maxWidth: '130px', maxHeight: '60px', minWidth: '130px', minHeight: '50px' }} >
+                    See Products
                 </Button>
+                <p id="intro">You have found the cleanest soap store on the internet. We pledge minimal processing, with maximal quality. To do this, we choose less processed ingredients, design immaculate formulations, and hand-make every batch of soap. With keen eyes for quality, and a sense of entrepreneurial duty, we aim to reduce energy waste in this world. The pale blue dot we call home, Earth, must be diligently protected -- Lucir strives for a clean body, mind, and soul.</p>
+                
+               
             </div>
-
         </div>
     );
 }
